428520aec03a84e89824649d9f0009ed75e194ea
inzapp <inzapp@naver.com>
2022-10-12 17:53:58 +0900

    add conversion verbose log

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -4,7 +4,12 @@ from pdf2image import convert_from_path
 
 for path in glob('*.pdf'):
     raw_name = os.path.basename(path)[:-4]
+    print(f'start converting {path}')
     pdf_pages = convert_from_path(path)
-    for i, pdf_page in enumerate(pdf_pages):
-        pdf_page.save(f'{raw_name}_{i}.jpg', 'JPEG')
+    print(f'conversion success')
+    for i in range(len(pdf_pages)):
+        img_path = f'{raw_name}_{i}.jpg'
+        pdf_pages[i].save(img_path, 'JPEG')
+        print(f'save image => {img_path}')
+    print()
 
