8d3dc6b405aec8540b4b890c51891c918908d0c2
inzapp <inzapp@naver.com>
2022-10-12 17:31:32 +0900

    convert all pdf file in current dir, use raw name instead of basename

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -1,10 +1,10 @@
 import os
+from glob import glob
 from pdf2image import convert_from_path
 
-file_name = 'pdf.pdf'
-basename = os.path.basename(file_name)
-pdf_pages = convert_from_path(file_name)
-
-for i, pdf_page in enumerate(pdf_pages):
-	pdf_page.save(f'{basename}_{i}.jpg', 'JPEG')
+for path in glob('*.pdf'):
+    raw_name = os.path.basename(path)[:-4]
+    pdf_pages = convert_from_path(path)
+    for i, pdf_page in enumerate(pdf_pages):
+        pdf_page.save(f'{raw_name}_{i}.jpg', 'JPEG')
 
